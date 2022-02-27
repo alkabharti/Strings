3dae141f92733855d91519542a5fa53e1909fadc
alka <alka16010@iiitd.ac.in>
2022-02-27 14:58:29 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,3 +1,4 @@
+Anagram : 
 Given two strings a and b consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. 
 An anagram of a string is another string that contains the same characters, only the order of characters can be different. 
 For example, act and tac are an anagram of each other.
@@ -14,3 +15,26 @@ Input:a = allergy, b = allergic
 Output: NO
 Explanation:Characters in both the strings are not same, so they are not anagrams.
   
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+  
+Solution : 
+
+Time Complexity : O(|a| + |b|)
+  
+public static boolean isAnagram(String a,String b)
+{
+    if(a.length()!=b.length())
+        return false;
+    else
+    {
+        char[] ch1=a.toCharArray();
+        char[] ch2=b.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        if(Arrays.equals(ch1,ch2))
+            return true;
+    }
+    return false;
+}
+
+
