ee48e49a8444c9e252bdd616c69ef522fbc95f5b
alka <alka16010@iiitd.ac.in>
2022-02-27 16:32:38 +0530

    Create Remove Duplicates.java

---
+++ Remove Duplicates.java
@@ -0,0 +1,27 @@
+Remove Duplicates:
+Given a string without spaces, the task is to remove duplicates from it.
+Note: The original order of characters must be kept the same. 
+
+Example 1:
+
+Input: S = "zvvo"
+Output: "zvo"
+Explanation: Only keep the first occurrence
+
+Example 2:
+
+Input: S = "gfg"
+Output: gf
+Explanation: Only keep the first occurrence
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+Solution 1 : Using ArrayList
+
+Time Complexity :
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+Solution 2 : Using HashSet
+
+Time Complexity :
